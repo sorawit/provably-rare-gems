509af66ec5a73b81267dd92bce915d1652ba2cbf
Sorawit Suriyakarn <thepsint@gmail.com>
2021-09-04 16:57:05 +0700

    More code

--- contracts/ProvablyRareGem.sol
+++ contracts/ProvablyRareGem.sol
@@ -4,115 +4,147 @@ pragma solidity 0.8.7;
 import 'OpenZeppelin/openzeppelin-contracts@4.3.0/contracts/token/ERC1155/extensions/ERC1155Supply.sol';
 import 'OpenZeppelin/openzeppelin-contracts@4.3.0/contracts/token/ERC721/IERC721.sol';
 import 'OpenZeppelin/openzeppelin-contracts@4.3.0/contracts/security/ReentrancyGuard.sol';
-import 'OpenZeppelin/openzeppelin-contracts@4.3.0/contracts/utils/math/Math.sol';
 
 import './Base64.sol';
-import './Gov.sol';
 
 /// @title Loot's Proably Rare Gems (for Adventurers)
 /// @author Sorawit Suriyakarn (swit.eth / https://twitter.com/nomorebear)
-contract ProvablyRareGem is ERC1155Supply, ReentrancyGuard, Gov {
+contract ProvablyRareGem is ERC1155Supply, ReentrancyGuard {
   IERC721 constant LOOT = IERC721(0xFF9C1b15B16263C61d017ee9F65C50e4AE0113D7);
 
   event Mine(address indexed minter, uint indexed kind);
   event Claim(uint indexed lootId, address indexed claimer);
 
+  struct Gem {
+    string name; // Gem name
+    string color; // Gem color
+    bool exists; // True if exist, False otherwise
+    uint difficulty; // Current difficulity level. Must be non decreasing
+    uint crafted; // Amount of gems crafted by the manager
+    uint multiplier; // Difficulty multiplier times 1e4. Must be between 1e4 and 1e10
+    uint craftCap; // Allocation to gem manager. Must be between 0 and 1e4
+    address manager; // Current gem manager
+    address pendingManager; // Pending gem manager to be transferred to
+  }
+
+  Gem[] public gems;
   mapping(address => uint) public nonce;
   mapping(uint => bool) public claimed;
-  uint[10] public crafted;
-  uint[10] public difficulty;
 
   constructor() ERC1155('n/a') {
-    for (uint kind = 0; kind < 10; kind++) {
-      difficulty[kind] = 8**kind;
+    gems[0] = Gem('Amethyst', '#9966CC', true, 8**0, 0, 10000, 1000, msg.sender, address(0));
+    gems[1] = Gem('Topaz', '#FFC87C', true, 8**1, 0, 10001, 1000, msg.sender, address(0));
+    gems[2] = Gem('Opal', '#A8C3BC', true, 8**2, 0, 10005, 1000, msg.sender, address(0));
+    gems[3] = Gem('Sapphire', '#0F52BA', true, 8**3, 0, 10010, 1000, msg.sender, address(0));
+    gems[4] = Gem('Ruby', '#E0115F', true, 8**4, 0, 10030, 1000, msg.sender, address(0));
+    gems[5] = Gem('Emerald', '#50C878', true, 8**5, 0, 10100, 1000, msg.sender, address(0));
+    gems[6] = Gem('Jadelite', '#00A36C', true, 8**6, 0, 10300, 1000, msg.sender, address(0));
+    gems[7] = Gem('Pink Diamond', '#FED0FC', true, 8**7, 0, 11000, 1000, msg.sender, address(0));
+    gems[8] = Gem('Blue Diamond', '#00A0FF', true, 8**8, 0, 20000, 1000, msg.sender, address(0));
+    gems[9] = Gem('Red Diamond', '#C50100', true, 8**9, 0, 50000, 1000, msg.sender, address(0));
+  }
+
+  /// @dev Mines new gemstones. Puts kind you want to mine + your salt and tests your luck!
+  function mine(uint kind, uint salt) external nonReentrant returns (uint) {
+    uint val = luck(kind, salt);
+    nonce[msg.sender]++;
+    require(gems[kind].exists, 'gem kind not exist');
+    uint diff = gems[kind].difficulty;
+    require(val <= type(uint).max / diff, 'salt not good enough');
+    gems[kind].difficulty = (diff * gems[kind].multiplier) / 10000;
+    _mint(msg.sender, kind, 1, '');
+  }
+
+  /// @dev Creates a new gem type. The manager that can craft a portion of gems + can premine
+  function create(
+    string calldata name,
+    string calldata color,
+    uint difficulty,
+    uint multiplier,
+    uint craftCap,
+    uint premine
+  ) external nonReentrant {
+    require(difficulty > 0, 'bad difficulty');
+    require(multiplier >= 1e4 && multiplier <= 1e10, 'bad multiplier');
+    require(craftCap < 1e4, 'bad craft cap');
+    uint kind = uint(keccak256(abi.encodePacked(msg.sender, name)));
+    require(!gems[kind].exists, 'gem kind already exists');
+    gems[kind] = Gem({
+      name: name,
+      color: color,
+      exists: true,
+      difficulty: difficulty,
+      crafted: 0,
+      multiplier: multiplier,
+      craftCap: craftCap,
+      manager: msg.sender,
+      pendingManager: address(0)
+    });
+    _mint(msg.sender, kind, premine, '');
+  }
+
+  /// @dev Transfer management ownership for the given gem kinds to another address.
+  function transferManager(uint[] calldata kinds, address to) external {
+    for (uint idx = 0; idx < kinds.length; idx++) {
+      uint kind = kinds[idx];
+      require(gems[kind].exists, 'gem kind not exist');
+      require(gems[kind].manager == msg.sender, 'not gem manager');
+      gems[kind].pendingManager = to;
     }
   }
 
-  /// @dev Mines new gemstones. Puts your salt and tests your luck!
-  function mine(uint salt) external nonReentrant returns (uint) {
-    uint val = luck(salt);
-    nonce[msg.sender]++;
-    for (uint kind = 9; kind >= 0; kind--) {
-      uint diff = difficulty[kind];
-      if (val <= type(uint).max / diff) {
-        difficulty[kind] = (difficulty[kind] * multiplier(kind)) / 10000;
-        for (uint id = 0; id <= kind; id++) {
-          _mint(msg.sender, id, 1, '');
-        }
-        emit Mine(msg.sender, kind);
-        return kind;
-      }
+  /// @dev Accepts management position for the given gem kinds.
+  function acceptManager(uint[] calldata kinds) external {
+    for (uint idx = 0; idx < kinds.length; idx++) {
+      uint kind = kinds[idx];
+      require(gems[kind].exists, 'gem kind not exist');
+      require(gems[kind].pendingManager == msg.sender, 'not gem manager');
+      gems[kind].pendingManager = address(0);
+      gems[kind].manager = msg.sender;
     }
-    assert(false);
   }
 
   /// @dev Called by LOOT owners to get welcome back of gems. Each loot ID can claim once.
   function claim(uint lootId) external nonReentrant {
     require(msg.sender == LOOT.ownerOf(lootId), 'not loot owner');
     require(claimed[lootId], 'already claimed');
-    claimed[lootId] = true;
-    for (uint kind = 0; kind < 5; kind++) {
-      _mint(msg.sender, kind, 1, '');
+    uint[4] memory kinds = airdrop(lootId);
+    for (uint idx = 0; idx < 4; idx++) {
+      _mint(msg.sender, kinds[idx], 1, '');
     }
     emit Claim(lootId, msg.sender);
   }
 
-  /// @dev Called by DAO governor to craft gems. Can't craft more than ceil[supply/10].
-  function craft(uint kind, uint amount) external gov nonReentrant {
-    require(kind < 10, 'bad kind');
-    crafted[kind] += amount;
+  /// @dev Returns the list of initial GEM distribution for the given loot ID.
+  function airdrop(uint lootId) public view returns (uint[4] memory kinds) {
+    uint count = 0;
+    for (uint kind = 9; kind > 0; kind--) {
+      uint seed = uint(keccak256(abi.encodePacked(kind, lootId)));
+      uint mod = [1, 1, 3, 6, 10, 20, 30, 100, 300, 1000][kind];
+      if (seed % mod == 0) {
+        kinds[count++] = kind;
+      }
+      if (count == 4) break;
+    }
+  }
+
+  /// @dev Called gem manager to craft gems. Can't craft more than supply*craftCap/10000.
+  function craft(uint kind, uint amount) external nonReentrant {
+    Gem storage gem = gems[kind];
+    require(gem.exists, 'gem kind not exist');
+    require(gem.manager == msg.sender, 'not gem manager');
+    gem.crafted += amount;
     _mint(msg.sender, kind, amount, '');
-    require(crafted[kind] <= Math.ceilDiv(totalSupply(kind), 10), 'too many crafts');
-  }
-
-  /// @dev Returns name of the given gemstone kind.
-  function gem(uint kind) public view returns (string memory) {
-    require(kind < 10, 'bad kind');
-    return
-      [
-        'Amethyst',
-        'Topaz',
-        'Opal',
-        'Sapphire',
-        'Ruby',
-        'Emerald',
-        'Jadelite',
-        'Pink Diamond',
-        'Blue Diamond',
-        'Red Diamond'
-      ][kind];
-  }
-
-  /// @dev Returns HEX color of the given gemstone kind.
-  function color(uint kind) public view returns (string memory) {
-    require(kind < 10, 'bad kind');
-    return
-      [
-        '#9966CC',
-        '#FFC87C',
-        '#A8C3BC',
-        '#0F52BA',
-        '#E0115F',
-        '#50C878',
-        '#00A36C',
-        '#FED0FC',
-        '#00A0FF',
-        '#C50100'
-      ][kind];
-  }
-
-  /// @dev Returns the difficulity multiplier once the given gem kind is mined, times 1e4.
-  function multiplier(uint kind) public view returns (uint) {
-    require(kind < 10, 'bad kind');
-    return [10000, 10001, 10005, 10010, 10030, 10100, 10300, 11000, 20000, 50000][kind];
+    require(gem.crafted <= (totalSupply(kind) * gem.craftCap) / 10000, 'too many crafts');
   }
 
   /// @dev Returns your luck given salt. The smaller the value, the better GEMs you will receive.
-  function luck(uint salt) public view returns (uint) {
+  function luck(uint kind, uint salt) public view returns (uint) {
     bytes memory data = abi.encodePacked(
       block.chainid,
       address(this),
       msg.sender,
+      kind,
       nonce[msg.sender],
       salt
     );
@@ -126,17 +158,18 @@ contract ProvablyRareGem is ERC1155Supply, ReentrancyGuard, Gov {
 
   // prettier-ignore
   function uri(uint kind) public view override returns (string memory) {
-    string[6] memory parts;
-    parts[0] = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: ';
-    parts[1] = color(kind);
-    parts[2] = '; font-family: serif; font-size: 14px; }</style><rect width="100%" height="100%" fill="white" /><text x="10" y="20" class="base">';
-    parts[3] = gem(kind);
-    parts[4] = '</text><text x="10" y="40" class="base">';
-    parts[5] = '</text></svg>';
-    string memory output = string(abi.encodePacked(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5]));
+    require(gems[kind].exists, 'gem kind not exist');
+    string memory output = string(abi.encodePacked(
+        '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: ',
+        gems[kind].color,
+        '; font-family: serif; font-size: 14px; }</style><rect width="100%" height="100%" fill="white" /><text x="10" y="20" class="base">',
+        gems[kind].name,
+        '</text><text x="10" y="40" class="base">',
+        '</text></svg>'
+    ));
     string memory json = Base64.encode(bytes(string(abi.encodePacked(
       '{ "name": "',
-      gem(kind),
+      gems[kind].name,
       '", ',
       '"description" : ',
       '"Loot\'s Provably Rare Gems (for Adventurers)", ',
--- tests/test_uri.py
+++ tests/test_uri.py
@@ -1,4 +1,17 @@
+from collections import Counter
 from brownie import ProvablyRareGem
+
+
+def test_claim2(a):
+    g = a[0].deploy(ProvablyRareGem)
+    c = Counter()
+    for i in range(200):
+        vals = g.claim2(i)
+        for v in vals:
+            c[v] += 1
+    for i in range(10):
+        print(i, c[i])
+    assert False
 
 
 def test_uri(a):
