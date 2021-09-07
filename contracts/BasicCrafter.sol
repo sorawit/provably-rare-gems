629787a4398b69230fe3bf0c8b66b3e00fa4b1ea
Sorawit Suriyakarn <thepsint@gmail.com>
2021-09-07 20:29:29 +0700

    Change gem name

--- contracts/BasicCrafter.sol
+++ contracts/BasicCrafter.sol
@@ -13,16 +13,16 @@ contract BasicGemCrafter is Ownable {
 
   constructor(ProvablyRareGemV2 _gem) {
     GEM = _gem;
-    _gem.create('Amethyst', '#9966CC', 8**2, 64, 10000, address(this), msg.sender);
-    _gem.create('Topaz', '#FFC87C', 8**3, 32, 10001, address(this), msg.sender);
-    _gem.create('Opal', '#A8C3BC', 8**4, 16, 10005, address(this), msg.sender);
-    _gem.create('Sapphire', '#0F52BA', 8**5, 8, 10010, address(this), msg.sender);
-    _gem.create('Ruby', '#E0115F', 8**6, 4, 10030, address(this), msg.sender);
-    _gem.create('Emerald', '#50C878', 8**7, 2, 10100, address(this), msg.sender);
-    _gem.create('Pink Diamond', '#FC74E4', 8**8, 1, 10300, address(this), msg.sender);
-    _gem.create('The Dragon Jade', '#00A36C', 8**9, 1, 11000, address(this), msg.sender);
-    _gem.create('Azure Skystone', '#348CFC', 8**10, 1, 20000, address(this), msg.sender);
-    _gem.create('Scarlet Bloodstone', '#BC1C2C', 8**11, 1, 50000, address(this), msg.sender);
+    _gem.create('Moonstone', '#', 8**2, 64, 10000, address(this), msg.sender);
+    _gem.create('Turquoise', '#', 8**3, 32, 10001, address(this), msg.sender);
+    _gem.create('Pearl', '#', 8**4, 16, 10005, address(this), msg.sender);
+    _gem.create('Sapphire of rarity', '#0F52BA', 8**5, 8, 10010, address(this), msg.sender);
+    _gem.create('Ruby of rarity', '#E0115F', 8**6, 4, 10030, address(this), msg.sender);
+    _gem.create('Emerald of rarity', '#50C878', 8**7, 2, 10100, address(this), msg.sender);
+    _gem.create('Blue Diamond', '#', 8**8, 1, 10300, address(this), msg.sender);
+    _gem.create("King's Crystal", '#', 8**9, 1, 11000, address(this), msg.sender);
+    _gem.create('Heart of The Dragon', '#', 8**10, 1, 20000, address(this), msg.sender);
+    _gem.create('Divine Sunstone', '#', 8**11, 1, 50000, address(this), msg.sender);
   }
 
   /// @dev Creaes more GEM
