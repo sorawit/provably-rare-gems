932bd2b4c1ba5d16ed92c3401a3647bcb40562bd
n1punp <disney31215@gmail.com>
2021-09-07 21:09:03 +0700

    Update gem names

--- contracts/BasicCrafter.sol
+++ contracts/BasicCrafter.sol
@@ -13,16 +13,18 @@ contract BasicGemCrafter is Ownable {
 
   constructor(ProvablyRareGemV2 _gem) {
     GEM = _gem;
-    _gem.create('Moonstone', '#', 8**2, 64, 10000, address(this), msg.sender);
-    _gem.create('Turquoise', '#', 8**3, 32, 10001, address(this), msg.sender);
-    _gem.create('Pearl', '#', 8**4, 16, 10005, address(this), msg.sender);
-    _gem.create('Sapphire of rarity', '#0F52BA', 8**5, 8, 10010, address(this), msg.sender);
-    _gem.create('Ruby of rarity', '#E0115F', 8**6, 4, 10030, address(this), msg.sender);
-    _gem.create('Emerald of rarity', '#50C878', 8**7, 2, 10100, address(this), msg.sender);
-    _gem.create('Blue Diamond', '#', 8**8, 1, 10300, address(this), msg.sender);
-    _gem.create("King's Crystal", '#', 8**9, 1, 11000, address(this), msg.sender);
-    _gem.create('Heart of The Dragon', '#', 8**10, 1, 20000, address(this), msg.sender);
-    _gem.create('Divine Sunstone', '#', 8**11, 1, 50000, address(this), msg.sender);
+    _gem.create('Turquoise', '#30D5C8', 8**2, 64, 10000, address(this), msg.sender);
+    _gem.create('Pearl', '#EAE0C8', 8**3, 32, 10001, address(this), msg.sender);
+    _gem.create('Zircon', '#007082', 8**4, 16, 10005, address(this), msg.sender);
+    _gem.create('Moonstone', '#A3C6C2', 8**5, 8, 10010, address(this), msg.sender);
+    _gem.create('Amber', '#FFBF00', 8**6, 4, 10030, address(this), msg.sender);
+    _gem.create('Spinel', '#034B03', 8**7, 2, 10100, address(this), msg.sender);
+    _gem.create('White Opal', '#7FFFD4', 8**8, 1, 10300, address(this), msg.sender);
+    _gem.create('Violet Garnet', '#872282', 8**9, 1, 11000, address(this), msg.sender);
+    _gem.create("Pheonix's Feather", '#F26722', 8**10, 1, 20000, address(this), msg.sender);
+    _gem.create('Yggdrasil Seed', '#21B54B', 8**11, 1, 50000, address(this), msg.sender);
+    _gem.create('Ocean Heart', '#1E88B4', 8**11, 1, 50000, address(this), msg.sender);
+    _gem.create("Philosopher's Stone", '#660000', 8**11, 1, 50000, address(this), msg.sender);
   }
 
   /// @dev Creaes more GEM
