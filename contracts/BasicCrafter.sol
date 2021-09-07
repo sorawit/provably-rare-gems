84dabc1501b4b8b8128aa95b3a4171f93d177ab6
Sorawit Suriyakarn <thepsint@gmail.com>
2021-09-07 19:50:53 +0700

    Remove pausable

--- contracts/BasicCrafter.sol
+++ contracts/BasicCrafter.sol
@@ -2,12 +2,11 @@
 pragma solidity 0.8.3;
 
 import 'OpenZeppelin/openzeppelin-contracts@4.3.0/contracts/access/Ownable.sol';
-import 'OpenZeppelin/openzeppelin-contracts@4.3.0/contracts/security/Pausable.sol';
 import './ProvablyRareGemV2.sol';
 
 /// @title Basic GEM Crafter
 /// @author Sorawit Suriyakarn (swit.eth / https://twitter.com/nomorebear)
-contract BasicGemCrafter is Ownable, Pausable {
+contract BasicGemCrafter is Ownable {
   ProvablyRareGemV2 public immutable GEM;
 
   mapping(uint => uint) public crafted;
@@ -37,32 +36,22 @@ contract BasicGemCrafter is Ownable, Pausable {
     GEM.create(name, color, difficulty, gemsPerMine, multiplier, address(this), msg.sender);
   }
 
-  /// @dev Pause crafter. Can only be called by owner.
-  function pause() external onlyOwner {
-    _pause();
-  }
-
-  /// @dev Unpause crafter. Can only be called by owner.
-  function unpause() external onlyOwner {
-    _unpause();
-  }
-
   /// @dev Called once to start mining for the given kinds.
-  function start(uint[] calldata kinds) external onlyOwner whenNotPaused {
+  function start(uint[] calldata kinds) external onlyOwner {
     for (uint idx = 0; idx < kinds.length; idx++) {
       GEM.updateEntropy(kinds[idx], blockhash(block.number - 1));
     }
   }
 
   /// @dev Called to stop mining for the given kinds.
-  function stop(uint kind) external onlyOwner whenNotPaused {
+  function stop(uint[] calldata kinds) external onlyOwner {
     for (uint idx = 0; idx < kinds.length; idx++) {
       GEM.updateEntropy(kinds[idx], bytes32(0));
     }
   }
 
   /// @dev Called by gem manager to craft gems. Can't craft more than 10% of supply.
-  function craft(uint kind, uint amount) external onlyOwner whenNotPaused {
+  function craft(uint kind, uint amount) external onlyOwner {
     require(amount != 0, 'zero amount craft');
     crafted[kind] += amount;
     GEM.craft(kind, amount, msg.sender);
