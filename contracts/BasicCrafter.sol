25667aed5937f041beb36947b9b85359fdd5a55d
Sorawit Suriyakarn <thepsint@gmail.com>
2021-09-07 21:31:30 +0700

    Adjust difficulty

--- contracts/BasicCrafter.sol
+++ contracts/BasicCrafter.sol
@@ -8,23 +8,22 @@ import './ProvablyRareGemV2.sol';
 /// @author Sorawit Suriyakarn (swit.eth / https://twitter.com/nomorebear)
 contract BasicGemCrafter is Ownable {
   ProvablyRareGemV2 public immutable GEM;
-
   mapping(uint => uint) public crafted;
 
   constructor(ProvablyRareGemV2 _gem) {
     GEM = _gem;
-    _gem.create('Turquoise', '#30D5C8', 8**2, 64, 10000, address(this), msg.sender);
-    _gem.create('Pearl', '#EAE0C8', 8**3, 32, 10001, address(this), msg.sender);
-    _gem.create('Zircon', '#007082', 8**4, 16, 10005, address(this), msg.sender);
-    _gem.create('Moonstone', '#A3C6C2', 8**5, 8, 10010, address(this), msg.sender);
-    _gem.create('Amber', '#FFBF00', 8**6, 4, 10030, address(this), msg.sender);
-    _gem.create('Spinel', '#034B03', 8**7, 2, 10100, address(this), msg.sender);
-    _gem.create('White Opal', '#7FFFD4', 8**8, 1, 10300, address(this), msg.sender);
-    _gem.create('Violet Garnet', '#872282', 8**9, 1, 11000, address(this), msg.sender);
-    _gem.create('Pheonix Feather', '#F26722', 8**10, 1, 20000, address(this), msg.sender);
-    _gem.create('Yggdrasil Seed', '#21B54B', 8**11, 1, 50000, address(this), msg.sender);
-    _gem.create('Ocean Heart', '#1E88B4', 8**11, 1, 50000, address(this), msg.sender);
-    _gem.create("Philosopher's Stone", '#660000', 8**11, 1, 50000, address(this), msg.sender);
+    _gem.create('Turquoise', '#30D5C8', 7**2, 64, 10000, address(this), msg.sender);
+    _gem.create('Pearl', '#EAE0C8', 7**3, 32, 10001, address(this), msg.sender);
+    _gem.create('Zircon', '#007082', 7**4, 16, 10005, address(this), msg.sender);
+    _gem.create('Moonstone', '#A3C6C2', 7**5, 8, 10010, address(this), msg.sender);
+    _gem.create('Amber', '#FFBF00', 7**6, 4, 10030, address(this), msg.sender);
+    _gem.create('Spinel', '#034B03', 7**7, 2, 10100, address(this), msg.sender);
+    _gem.create('White Opal', '#7FFFD4', 7**8, 1, 10300, address(this), msg.sender);
+    _gem.create('Violet Garnet', '#872282', 7**9, 1, 11000, address(this), msg.sender);
+    _gem.create('Pheonix Feather', '#F26722', 7**10, 1, 13000, address(this), msg.sender);
+    _gem.create('Yggdrasil Seed', '#21B54B', 7**11, 1, 15000, address(this), msg.sender);
+    _gem.create('Ocean Heart', '#1E88B4', 7**12, 1, 20000, address(this), msg.sender);
+    _gem.create("Philosopher's Stone", '#660000', 7**13, 1, 30000, address(this), msg.sender);
   }
 
   /// @dev Creaes more GEM
