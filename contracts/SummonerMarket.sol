5ed5ef63ea736748fc41672bc14b5cc3e7baa1cc
Sorawit Suriyakarn <thepsint@gmail.com>
2021-09-09 18:23:44 +0700

    Add events

--- contracts/SummonerMarket.sol
+++ contracts/SummonerMarket.sol
@@ -9,6 +9,10 @@ import 'OpenZeppelin/openzeppelin-contracts@4.3.0/contracts/proxy/utils/Initiali
 /// @author swit.eth (@nomorebear) + nipun (@nipun_pit) + jade (@jade_arin)
 contract SummonnerMarket is Initializable, ERC721Holder {
   event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+  event List(uint indexed id, address indexed lister, uint price);
+  event Unlist(uint indexed id, address indexed lister);
+  event Buy(uint indexed id, address indexed seller, address indexed buyer, uint price, uint fee);
+  event SetFeeBps(uint feeBps);
 
   IERC721 public rarity;
   uint public feeBps;
@@ -44,6 +48,7 @@ contract SummonnerMarket is Initializable, ERC721Holder {
 
   function setFeeBps(uint _feeBps) external onlyOwner {
     feeBps = _feeBps;
+    emit SetFeeBps(_feeBps);
   }
 
   function list(uint summonerId, uint price) external nonReentrant {
@@ -52,6 +57,7 @@ contract SummonnerMarket is Initializable, ERC721Holder {
     rarity.safeTransferFrom(msg.sender, address(this), summonerId);
     prices[summonerId] = price;
     listers[summonerId] = msg.sender;
+    emit List(summonerId, msg.sender, price);
   }
 
   function unlist(uint summonerId) external nonReentrant {
@@ -60,6 +66,7 @@ contract SummonnerMarket is Initializable, ERC721Holder {
     prices[summonerId] = 0;
     listers[summonerId] = address(0);
     rarity.safeTransferFrom(address(this), msg.sender, summonerId);
+    emit Unlist(summonerId, msg.sender);
   }
 
   function buy(uint summonerId) external payable nonReentrant {
@@ -68,10 +75,12 @@ contract SummonnerMarket is Initializable, ERC721Holder {
     require(msg.value == price, 'bad msg.value');
     uint fee = (price * feeBps) / 10000;
     uint get = price - fee;
+    address lister = listers[summonerId];
     prices[summonerId] = 0;
     listers[summonerId] = address(0);
     rarity.safeTransferFrom(address(this), msg.sender, summonerId);
-    payable(listers[summonerId]).transfer(get);
+    payable(lister).transfer(get);
+    emit Buy(summonerId, lister, msg.sender, price, fee);
   }
 
   function withdraw(uint amount) external onlyOwner {
