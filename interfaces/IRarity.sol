ff3706b10d3d2c707fda1a1a12bf128c68f42995
n1punp <disney31215@gmail.com>
2021-09-15 21:47:41 +0700

    Add interface

---
+++ interfaces/IRarity.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.3;
+
+interface IRarity {
+  function next_summoner() external returns (uint);
+
+  function summon(uint _class) external;
+
+  function ownerOf(uint) external view returns (address);
+
+  function getApproved(uint) external view returns (address);
+}
